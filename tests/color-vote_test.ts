874dcad5bf4f1c0b3cb1221348e6a1e968a374d9
Hugo Caillard <hg.caillard@gmail.com>
2022-03-11 19:34:17 +0100

    add fourth step - automatic testing

--- tests/color-vote_test.ts
+++ tests/color-vote_test.ts
@@ -1,26 +1,61 @@
+import {
+  Chain,
+  Clarinet,
+  Account,
+  Tx,
+  types,
+} from 'https://deno.land/x/clarinet@v0.27.0/index.ts'
 
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+const { uint } = types
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+  name: '`get-nb-of-voters` - returns the right number of voters',
+  fn(chain: Chain, accounts: Map<string, Account>) {
+    const { address } = accounts.get('wallet_1')!
+    const { receipts } = chain.mineBlock([
+      Tx.contractCall('color-vote', 'get-nb-of-voters', [], address),
+    ])
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
-});
+    receipts[0].result.expectUint(0)
+  },
+})
+
+Clarinet.test({
+  name: '`vote` - participant can vote only one time',
+  fn(chain: Chain, accounts: Map<string, Account>) {
+    const { address } = accounts.get('wallet_1')!
+    const { receipts } = chain.mineBlock([
+      Tx.contractCall('color-vote', 'vote', [5, 5, 5, 5].map(uint), address),
+      Tx.contractCall('color-vote', 'vote', [5, 5, 5, 5].map(uint), address),
+    ])
+
+    receipts[0].result.expectOk().expectBool(true)
+    receipts[1].result.expectErr().expectUint(403)
+  },
+})
+
+Clarinet.test({
+  name: '`vote` - vote increments the number of voters',
+  fn(chain: Chain, accounts: Map<string, Account>) {
+    const { address } = accounts.get('wallet_1')!
+    const { receipts } = chain.mineBlock([
+      Tx.contractCall('color-vote', 'vote', [5, 5, 5, 5].map(uint), address),
+      Tx.contractCall('color-vote', 'get-nb-of-voters', [], address),
+    ])
+
+    receipts[0].result.expectOk().expectBool(true)
+    receipts[1].result.expectUint(1)
+  },
+})
+
+Clarinet.test({
+  name: '`vote` - throw an error if the vote is not valid',
+  fn(chain: Chain, accounts: Map<string, Account>) {
+    const { address } = accounts.get('wallet_1')!
+    const { receipts } = chain.mineBlock([
+      Tx.contractCall('color-vote', 'vote', [6, 5, 5, 5].map(uint), address),
+    ])
+
+    receipts[0].result.expectErr().expectUint(400)
+  },
+})
